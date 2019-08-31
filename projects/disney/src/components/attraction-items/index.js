ceb9f4845f5ec4da8044f66d27691f8f129caf09
Kuma Li <li.jun.kuma@gmail.com>
2019-09-01 01:07:19 +0800

    [disney] fix attraction render bugs and css

--- projects/disney/src/components/attraction-items/index.js
+++ projects/disney/src/components/attraction-items/index.js
@@ -3,6 +3,18 @@ import { View, Image, Text } from '@tarojs/components'
 import './index.scss'
 
 class AttractionItems extends Component {
+  renderDefiniton(definitions) {
+    if (definitions) {
+      return (
+        <View className='definition'>
+          {definitions.slice().map(definition => (
+            <View key={`${definition.name}${definition.value}`}>{definition.name}: {definition.value}</View>
+          ))}
+        </View>
+      )
+    }
+    return null;
+  }
   render () {
     const { items, type } = this.props
     let clsName = 'list-item'
@@ -12,17 +24,13 @@ class AttractionItems extends Component {
     if (type == 'character') {
       clsName += ' list-item character'
     }
-    return items.map(item => (
-      <View className={clsName} key={item.name}>
+    return items && items.map(item => (
+      <View className={clsName} key={`${item.area}${item.name}`}>
         <Image className='list-image' lazyLoad mode='aspectFill' src={item.src} alt={item.alt} />
         <View className='list-text'>
           <View className='list-item-name'>{item.name}</View>
           {item.desc ? <View className='list-item-desc'>{item.desc}</View> : null}
-          <View className='definition'>
-            {item.definitions.map(definition => (
-              <View key={definition.name}>{definition.name}: {definition.value}</View>
-            ))}
-          </View>
+          {this.renderDefiniton(item.definitions)}
           <View className='list-item-hot'>
             {item.iconTag && type == 'attraction' ? <Text className='list-item-fastpass'>FASTPASS</Text> : null}
             {item.iconTag && type != 'attraction' ? <Text className='list-item-fastpass'>{item.iconTag}</Text> : null}
